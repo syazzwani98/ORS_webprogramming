f0d0a9cdf1ff9e196460a8c1fad5b0a4108553d7
syazzwani98 <62368415+syazzwani98@users.noreply.github.com>
2021-01-18 23:19:06 +0800

    Add files via upload

---
+++ AccApply.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<title>Online Residential College</title>
+<html>
+    <head>
+            <meta charset="utf-8">
+        
+
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+        <style>
+            h1{
+                color:white;
+                padding-left: 10px;
+            }
+            h2{
+                padding-top: 10px;
+            }
+            header{
+                padding-top: 40px;
+                padding-bottom: 235px;
+                background-color: lightblue;
+                background-image: url("birds.png");
+            }
+        
+            nav{
+                place-content: center;
+            }
+            .jumbotron {
+            background-color: #e3f2fd;
+            }
+            .img{
+                text-align: center;
+                display: block;
+            }
+            .material-icons{
+                vertical-align: middle;
+                padding-bottom: 5px;
+                font-size: 150px;
+            }
+            
+        </style>
+
+    </head>
+    <body>
+            <header>
+                    <div id="header" class="section">
+                        <!--img alt="" class="img-circle" src="https://code.sololearn.com/Icons/Avatars/0.jpg"-->
+                        <h1 id="1"><b><p>Welcome to 9th Residential College </p></b></h1>
+                    </div>
+            </header>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark section">
+      
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+          
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+          
+                            <li class="nav-item">
+                                <a class ="nav-link" href="Signed-In-Page.html">HOME<span class="sr-only"></span></a>
+                            </li> 
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="CollegeAccommodation.php">ACCOMMODATION<span class="sr-only"></span></a>
+                            </li>
+
+                            <li class="nav-item active">
+                                <a class="nav-link" href="AccApply.php">APPLICATION<span class="sr-only"></span></a>
+                            </li>
+
+                        </ul  >
+                    </div>
+                    
+                    <ul class="nav nav-pills">
+                      <li class="nav-item">
+                        <a class="nav-link active" href="LOGINNNN.html">Logout</a>
+                      </li>
+ 
+                    </ul>
+                  </nav>
+
+    <hr>
+             
+    <div class="jumbotron">
+        <main>
+            <div class="card-group">
+                <div class="card">
+                  <span class="img"><i class="material-icons">airline_seat_individual_suite</i>
+                  <div class="card-body">
+                    <h5 class="card-title text-center">ROOM APPLICATION</h5>
+                    <a class="btn btn-info" href="room.php" role="button">APPLY</a>
+                  </div>
+                </span>
+                </div>
+                <div class="card">
+                    <span class="img"><i class="material-icons">meeting_room</i>
+                  <div class="card-body">
+                    <h5 class="card-title text-center">FACILITIES APPLICATION</h5>
+                    <a class="btn btn-info" href="facilities.php" role="button">APPLY</a>
+                  </div>
+                </span>
+                </div>
+              </div>
+        </main>
+    </div>
+
+    <span class="img"><a class="btn btn-primary" href="CollegeAccommodation.html" role="button">BACK TO ACCOMMODATION</a></span>
+
+    <hr>
+        <footer class="container text-center font-italic">
+                        <p style="margin:0;">
+                            Copyright &copy; 2019 this is the footer section</p>
+                            <p style="margin:0;">
+                                <a href="spectrum.um.edu.my">spectrum.um.edu.my</a>
+                        </p>
+        </footer>
+   
+<!--jQuery-->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+integrity="sha384-
+q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
+crossorigin="anonymous"></script>
+
+<!--Popper.js-->
+<script 
+src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/pop
+per.min.js" integrity="sha384-
+UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" 
+crossorigin="anonymous"></script>
+
+<!--Bootstrap.Js-->
+<script 
+src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap
+.min.js" integrity="sha384-
+JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" 
+crossorigin="anonymous"></script>
+
+    </body>
+</html>
